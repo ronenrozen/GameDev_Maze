16833b6f1d22326b56c8ea47f03b2ba8a9c1840a
ronenrozen92 <ronenr4@mail.afeka.ac.il>
2020-09-29 20:25:16 +0300

    GameDev_Maze

---
+++ Assets/DoorAutomation.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class DoorAutomation : MonoBehaviour
+{
+    public GameObject camera;
+    public int threshold = 100;
+    int isOutside = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        camera = GameObject.FindGameObjectWithTag("MainCamera");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 cameraPosition = camera.transform.position;
+        Vector3 doorPosition = this.transform.position;
+        float angleX = transform.rotation.eulerAngles.x; 
+        float angleY = transform.rotation.eulerAngles.y; 
+        float angleZ = transform.rotation.eulerAngles.z; 
+        if (isClose(cameraPosition,doorPosition)){
+
+            if (angleY < 0 || angleY > 90) {
+                isOutside = isOutside * -1;
+            }
+            
+            transform.Rotate(0f,10f * isOutside * Time.deltaTime,0f);
+            
+        }
+    }
+
+    bool isClose(Vector3 cameraPosition,Vector3 doorPosition )
+    {
+        return Math.Abs(cameraPosition.x - doorPosition.x) < threshold && Math.Abs(cameraPosition.y - doorPosition.y) < threshold && Math.Abs(cameraPosition.z - doorPosition.z) < threshold;
+    }
+}
---
+++ Assets/Scripts/MotionDoorLeft.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class MotionDoorLeft : MonoBehaviour
+{
+    public GameObject camera;
+    public GameObject door;
+    public int threshold = 20;
+    int isOutside = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        camera = GameObject.FindGameObjectWithTag("MainCamera");
+        door = GameObject.FindGameObjectWithTag("leftDoor");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 cameraPosition = camera.transform.position;
+
+        Vector3 doorPosition = this.transform.position;
+
+        if (isClose(cameraPosition,doorPosition)) {
+
+            if (doorPosition.x < -20 || doorPosition.x > -10) {
+                isOutside = isOutside * - 1;
+            }
+            Vector3 newPos =  new Vector3(4f * isOutside * Time.deltaTime,0,0); 
+            door.transform.position += newPos; 
+            
+        }
+    }
+
+    bool isClose(Vector3 cameraPosition,Vector3 doorPosition )
+    {
+        return Math.Abs(cameraPosition.x - doorPosition.x) < threshold && Math.Abs(cameraPosition.y - doorPosition.y) < threshold && Math.Abs(cameraPosition.z - doorPosition.z) < threshold;
+    }
+}
